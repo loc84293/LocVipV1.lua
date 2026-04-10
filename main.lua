4ea34e04e34aaaa6822eb0940229293c5dd7c7ea
loc84293 <loc337487@gmail.com>
2026-04-10 17:36:27 +0700

    Change brand name and comments in main.lua
    Updated branding information in main.lua with comments.

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
--- [[ 👑 LỘC VIP V1 👑 ]] --
+--- [[ 👑 LỘC VIP V1 👑 ]] --
+-- NGUYÊN BẢN ANHTUANDZAI HUB (TUAN ANH IOS)
+-- CHỈ THAY ĐỔI TÊN THƯƠNG HIỆU - GIỮ NGUYÊN CODE GỐC
+
 local LocVip_Brand = "👑 LỘC VIP V1"
 
 -- [ 🚀 TẢI VÀ XỬ LÝ MÃ NGUỒN ] --
