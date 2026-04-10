015c808e2e961806633b5b88c782259453c64d7d
loc84293 <loc337487@gmail.com>
2026-04-10 17:38:50 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
---- [[ 👑 LỘC VIP V1 👑 ]] --
+-- [[ 👑 LỘC VIP V1 👑 ]] --
+
 local LocVip_Brand = "👑 LỘC VIP V1"
+
 -- [ 🚀 TẢI VÀ XỬ LÝ MÃ NGUỒN ] --
 local Success, RawCode = pcall(function()
     return game:HttpGet("https://raw.githubusercontent.com/AnhTuanDzai-Hub/TuanAnhIOS/refs/heads/main/TuanAnhIOS-Main.Lua")
